fc97ff5be047c8d4f7f0d3897ec1573f00a8c0b7
Akash JS <akashjs710@gmail.com>
2023-04-24 10:55:24 +0530

    main file config

---
+++ api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes(app *fiber.app) {
+	app.Get("/:url", routes.ResolveURL)
+	app.POST("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	app := fiber.New()
+	app.Use(logger.New())
+
+	setupRoutes(app)
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+}
