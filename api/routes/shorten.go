815ccf2253f45f702ada9569d50ab4445bac1ddf
Akash JS <akashjs710@gmail.com>
2023-04-25 13:05:07 +0530

    rate limiting setup

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,11 +1,15 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
+	"github.com/jsakash/shorten-url-fier-redis/database"
 	"github.com/jsakash/shorten-url-fier-redis/helpers"
+	"github.com/redis/go-redis/v9"
 )
 
 type request struct {
@@ -31,6 +35,23 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// implement rate limiting
 
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QOUTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate Limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
+
 	//check if the input is an actual URL
 	if !govalidator.IsURL(body.URL) {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
@@ -44,4 +65,6 @@ func ShortenURL(c *fiber.Ctx) error {
 	// enforce https, SSL
 
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	r2.Decr(database.Ctx, c.IP())
 }
